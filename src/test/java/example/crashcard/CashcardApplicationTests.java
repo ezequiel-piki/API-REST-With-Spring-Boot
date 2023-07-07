fcec0ff26c23ff46c4110d819f3d0c6bb3e29ef0
ezequiel-piki <ezequielpiki23@gmail.com>
2023-07-07 16:03:50 -0300

    Agregamos el endpoint para crear nuestra cashcard en el controlador

--- src/test/java/example/crashcard/CashcardApplicationTests.java
+++ src/test/java/example/crashcard/CashcardApplicationTests.java
@@ -42,17 +42,20 @@ class CashcardApplicationTests {
         @Test
         public void shouldCreateANewCashCard(){
         
-            CashCard newCashCard = new CashCard(null, 250.00 );
+            CashCard newCashCard = new CashCard(44L, 250.00 );
             ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
             assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-
-
 
             URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
             ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard,String.class);
             assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
-            
-            
+
+            DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+            Number id = documentContext.read("$.id");
+            Double amount = documentContext.read("$.amount");
+
+            assertThat(id).isNotNull();
+            assertThat(amount).isEqualTo(250.00);
         }
   
 	@Test
